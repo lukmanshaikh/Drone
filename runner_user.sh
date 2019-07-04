56503261f4eadfca3f084bc318ee776c6728c214
YasiR Siddiqui <38703416+Yasir-siddiqui@users.noreply.github.com>
2019-07-04 13:02:00 +0530

    ;_;

--- runner_user.sh
+++ runner_user.sh
@@ -16,22 +16,14 @@ build_env()
 . "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/telegram
 TELEGRAM_TOKEN=$(cat /tmp/tg_token)
 export TELEGRAM_TOKEN
-tg_sendinfo "<code>[MaestroCI]: GCC-10 Compiler Job rolled!</code>"
+tg_sendinfo "<code>[BoT]: Meh new build is Starting!</code>"
 cd ~
 git clone https://github.com/akhilnarang/scripts > /dev/null 2>&1
 cd scripts
 bash setup/android_build_env.sh  > /dev/null 2>&1
 cd ..
 rm -rf scripts
-sudo apt install help2man libtool-bin wget -y > /dev/null 2>&1
-git clone https://github.com/crosstool-ng/crosstool-ng > /dev/null 2>&1
-cd crosstool-ng
-./bootstrap > /dev/null 2>&1
-./configure > /dev/null 2>&1
-make -j$(nproc) > /dev/null 2>&1
-sudo make install > /dev/null 2>&1
-cd ..
-rm -rf crosstool-ng
+
 echo "Build Dependencies Installed....."
 }
 ##### Build Configs
@@ -40,32 +32,30 @@ build_conf()
 {
 mkdir repo
 cd repo
-git config --global user.email "baalajimaestro@computer4u.com"
-git config --global user.name "baalajimaestro"
+git config --global user.email "www.mohammad.yasir.s@gmail.com"
+git config --global user.name "Yasir-siddiqui"
+git config --global color.ui false
 export LOC=$(cat /tmp/loc)
 }
 
 run()
 {
 echo "Starting build!"
-git clone https://github.com/baalajimaestro/ct-ng-configs -b GCC-10 > /dev/null 2>&1
-cd ct-ng-configs
-ct-ng build > /dev/null 2>&1
+repo init --depth=1 -u https://github.com/ArrowOS/android_manifest.git -b arrow-9.x > /dev/null 2>&1
+repo sync  -f --force-sync --no-clone-bundle -j8 > /dev/null 2>&1
+git clone https://github.com/ArrowOS-Devices/android_device_asus_X00TD device/asus/X00TD
+. build/envsetup.sh 
+brunch X00TD
+cd out/target/product/X00TD
+curl -F'file=@Arrow-v9.0-X00TD-OFFICIAL-*.zip' https://0x0.st
+
 echo "Build finished!"
 }
 
-##### Here's the blecc megik
+##### Here's the blecc megik k
 push_gcc()
 {
-sudo chmod -R 777 $HOME/x-tools
-cd /home/ci/x-tools/aarch64-maestro-linux-gnu
-git init
-git add .
-git checkout -b $(date +%d%m%y)
-git commit -m "[MaestroCI]: GCC-10 $(date +%d%m%y)" --signoff
-git remote add origin https://baalajimaestro:$(cat /tmp/GH_TOKEN)@github.com/baalajimaestro/aarch64-maestro-linux-android.git
-git push --force origin $(date +%d%m%y)
-tg_sendinfo "<code>Checked out and pushed GCC-10</code>"
+tg_sendinfo "<code>Check Out New Build ;_;</code>"
 echo "Job Successful!"
 }
 
